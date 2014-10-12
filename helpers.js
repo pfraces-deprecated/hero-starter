aaef613a307d93591c035074a2598ddb119acee6
Pau Fracés <pfraces@gmail.com>
2014-10-12 03:34:00 +0200

    refactored using revealing module pattern

--- helpers.js
+++ helpers.js
@@ -1,45 +1,52 @@
-var helpers = {};
-
-// Returns false if the given coordinates are out of range
-helpers.validCoordinates = function(board, distanceFromTop, distanceFromLeft) {
+/**
+ * validCoordinates()
+ * 
+ * Returns false if the given coordinates are out of range
+ */
+ 
+var validCoordinates = function (board, distanceFromTop, distanceFromLeft) {
   return (!(distanceFromTop < 0 || distanceFromLeft < 0 ||
       distanceFromTop > board.lengthOfSide - 1 || distanceFromLeft > board.lengthOfSide - 1));
 };
 
-// Returns the tile [direction] (North, South, East, or West) of the given X/Y coordinate
-helpers.getTileNearby = function(board, distanceFromTop, distanceFromLeft, direction) {
+/**
+ * getTileNearby()
+ * 
+ * Returns the tile [direction] (North, South, East, or West) of the given X/Y coordinate
+ */
+ 
+var getTileNearby = function (board, distanceFromTop, distanceFromLeft, direction) {
 
   // These are the X/Y coordinates
+  
   var fromTopNew = distanceFromTop;
   var fromLeftNew = distanceFromLeft;
 
   // This associates the cardinal directions with an X or Y coordinate
-  if (direction === 'North') {
-    fromTopNew -= 1;
-  } else if (direction === 'East') {
-    fromLeftNew += 1;
-  } else if (direction === 'South') {
-    fromTopNew += 1;
-  } else if (direction === 'West') {
-    fromLeftNew -= 1;
-  } else {
-    return false;
-  }
+  
+  if (direction === 'North') { fromTopNew -= 1; }
+  else if (direction === 'East') { fromLeftNew += 1; }
+  else if (direction === 'South') { fromTopNew += 1; }
+  else if (direction === 'West') { fromLeftNew -= 1; }
+  else { return false; }
 
   // If the coordinates of the tile nearby are valid, return the tile object at those coordinates
-  if (helpers.validCoordinates(board, fromTopNew, fromLeftNew)) {
-    return board.tiles[fromTopNew][fromLeftNew];
-  } else {
-    return false;
-  }
+  
+  if (validCoordinates(board, fromTopNew, fromLeftNew)) { return board.tiles[fromTopNew][fromLeftNew]; }
+  return false;
 };
 
-// Returns an object with certain properties of the nearest object we are looking for
-helpers.findNearestObjectDirectionAndDistance = function(board, fromTile, tileCallback) {
+/**
+ * findNearestObjectDirectionAndDistance()
+ * 
+ * Returns an object with certain properties of the nearest object we are looking for
+ */
+ 
+var findNearestObjectDirectionAndDistance = function (board, fromTile, tileCallback) {
   // Storage queue to keep track of places the fromTile has been
   var queue = [];
 
-  //Keeps track of places the fromTile has been for constant time lookup later
+  // Keeps track of places the fromTile has been for constant time lookup later
   var visited = {};
 
   // Variable assignments for fromTile's coordinates
@@ -49,7 +56,7 @@ helpers.findNearestObjectDirectionAndDistance = function(board, fromTile, tileCa
   // Stores the coordinates, the direction fromTile is coming from, and it's location
   var visitInfo = [dft, dfl, 'None', 'START'];
 
-  //Just a unique way of storing each location we've visited
+  // Just a unique way of storing each location we've visited
   visited[dft + '|' + dfl] = true;
 
   // Push the starting tile on to the queue
@@ -67,13 +74,14 @@ helpers.findNearestObjectDirectionAndDistance = function(board, fromTile, tileCa
 
     // Loop through cardinal directions
     var directions = ['North', 'East', 'South', 'West'];
+    
     for (var i = 0; i < directions.length; i++) {
 
       // For each of the cardinal directions get the next tile...
       var direction = directions[i];
 
       // ...Use the getTileNearby helper method to do this
-      var nextTile = helpers.getTileNearby(board, dft, dfl, direction);
+      var nextTile = getTileNearby(board, dft, dfl, direction);
 
       // If nextTile is a valid location to move...
       if (nextTile) {
@@ -91,9 +99,9 @@ helpers.findNearestObjectDirectionAndDistance = function(board, fromTile, tileCa
         // If we have visited this tile before
         if (visited.hasOwnProperty(key)) {
 
-          //Do nothing--this tile has already been visited
+          // Do nothing--this tile has already been visited
 
-        //Is this tile the one we want?
+        // Is this tile the one we want?
         } else if (isGoalTile) {
 
           // This variable will eventually hold the first direction we went on this path
@@ -141,50 +149,48 @@ helpers.findNearestObjectDirectionAndDistance = function(board, fromTile, tileCa
   return false;
 };
 
-// Returns the nearest health well or false, if there are no health wells
-helpers.findNearestHealthWell = function(gameData) {
+/**
+ * findNearestHealthWell()
+ * 
+ * Returns the nearest health well or false, if there are no health wells
+ */
+ 
+var findNearestHealthWell = function (gameData) {
   var hero = gameData.activeHero;
   var board = gameData.board;
 
-  //Get the path info object
-  var pathInfoObject = helpers.findNearestObjectDirectionAndDistance(board, hero, function(healthWellTile) {
+  // Get the path info object
+  var pathInfoObject = findNearestObjectDirectionAndDistance(board, hero, function (healthWellTile) {
     return healthWellTile.type === 'HealthWell';
   });
 
-  //Return the direction that needs to be taken to achieve the goal
+  // Return the direction that needs to be taken to achieve the goal
   return pathInfoObject.direction;
 };
 
-// Returns the direction of the nearest enemy with lower health
-// (or returns false if there are no accessible enemies that fit this description)
-helpers.findNearestWeakerEnemy = function(gameData) {
+/**
+ * findNearestWeakerEnemy()
+ * 
+ * Returns the direction of the nearest enemy with lower health
+ * (or returns false if there are no accessible enemies that fit this description)
+ */
+ 
+var findNearestWeakerEnemy = function (gameData) {
   var hero = gameData.activeHero;
   var board = gameData.board;
 
-  //Get the path info object
-  var pathInfoObject = helpers.findNearestObjectDirectionAndDistance(board, hero, function(enemyTile) {
+  // Get the path info object
+  var pathInfoObject = findNearestObjectDirectionAndDistance(board, hero, function (enemyTile) {
     return enemyTile.type === 'Hero' && enemyTile.team !== hero.team && enemyTile.health < hero.health;
   });
 
-  //Return the direction that needs to be taken to achieve the goal
-  //If no weaker enemy exists, will simply return undefined, which will
-  //be interpreted as "Stay" by the game object
+  // Return the direction that needs to be taken to achieve the goal
+  // If no weaker enemy exists, will simply return undefined, which will
+  // be interpreted as "Stay" by the game object
   return pathInfoObject.direction;
 };
 
-// Returns the direction of the nearest enemy
-// (or returns false if there are no accessible enemies)
-helpers.findNearestEnemy = function(gameData) {
-  var hero = gameData.activeHero;
-  var board = gameData.board;
-
-  //Get the path info object
-  var pathInfoObject = helpers.findNearestObjectDirectionAndDistance(board, hero, function(enemyTile) {
-    return enemyTile.type === 'Hero' && enemyTile.team !== hero.team
-  });
-
-  //Return the direction that needs to be taken to achieve the goal
-  return pathInfoObject.direction;
+module.exports = {
+  findNearestHealthWell: findNearestHealthWell,
+  findNearestWeakerEnemy: findNearestWeakerEnemy
 };
-
-module.exports = helpers;
